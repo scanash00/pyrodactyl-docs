b87609073e65bee668a1bafd1be1b784f92c2ddc
Naterfute <naterfute@pyro.host>
2025-09-08 01:45:43 -0700

    feat: fully static app(for cloudflare pages :yippee:

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,23 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  output: "export",
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'camo.githubusercontent.com'
+        protocol: "https",
+        hostname: "camo.githubusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i.imgur.com'
-      }
-    ]
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+    ],
   },
 };
 
