5ce02ecb3fd711f8b6be00799a41b5220da6954f
Scan <103391616+scanash00@users.noreply.github.com>
2025-10-11 22:47:14 -0800

    fix: point to docs on / navbar

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -39,7 +39,7 @@ export const baseOptions: BaseLayoutProps = {
   links: [
     {
       text: 'Documentation',
-      url: '/docs',
+      url: '/docs/pyrodactyl',
       active: 'nested-url',
     },
   ],
